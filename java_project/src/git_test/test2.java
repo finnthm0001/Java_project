ea5b9d6a638f5fa897b586838952b130ab67621f
kang <kang@kang>
2019-11-10 21:07:48 +0900

    두번째 테스트

--- java_project/src/git_test/test2.java
+++ java_project/src/git_test/test2.java
@@ -1,12 +1,10 @@
 package git_test;
 
-public class test1 {
+public class test2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		// 테스트
-		// 2번째 추가
-
+		// test2-1
 	}
 
 }
